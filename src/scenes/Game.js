eb87408b013a51cc6fe2ef205ae04a6cffd2d6b0
Devaun Nicholson <devaunnicholson@Devauns-MacBook-Air-2.local>
2022-03-17 22:55:20 -0700

    second commit

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,11 +1,25 @@
 import Phaser from "phaser";
 // assets
 import bg from "../assets/img/background-iceberg.png";
+import ship from "../assets/img/ship.png";
+import bulletImg from "../assets/img/final_duc.jpg"
 
+import Robot from "../objects/Player";
+class BulletGroup extends Phaser.Physics.Group{
+  
+}
 export default class Game extends Phaser.Scene {
+
+  constructor() {
+    super("playGame")
+  }
+
+
   preload() {
     // load image
     this.load.image("background", `${bg}`);
+    this.load.image("player", `${ship}`)
+    this.load.image("bullet", `${bulletImg}`)
   }
 
   create() {
@@ -14,7 +28,20 @@ export default class Game extends Phaser.Scene {
       .image(0, 0, "background")
       .setOrigin(0, 0)
       .setDisplaySize(800, 600);
-  }
 
-  update() {}
+    this.player = new Robot(this, 0, 100, "player");
+    this.player.setScale(0.1);
+    this.physics.add.existing(this.player);
+    this.physics.world.setBounds(0, 0, this.background.displayWidth, this.background.displayHeight);
+    this.player.body.setCollideWorldBounds(true);
+    
+    this.input = this.input.keyboard.createCursorKeys();
+  }  
+
+
+
+
+  update() {
+    this.player.update(this.input);
+ }
 }
